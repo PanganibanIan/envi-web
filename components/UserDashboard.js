22a94c09d1bb36e1cae54be5788d7d6d35ac4f42
ian-p.panganiban <ian-p.panganiban@gcash.com>
2023-08-04 13:05:38 +0800

    User Dashboard basic infos

--- components/UserDashboard.js
+++ components/UserDashboard.js
@@ -2,63 +2,110 @@ import FetchData from '@/hooks/FetchData';
 import React, { useState, useEffect } from 'react';
 
 function UserDashboard() {
-// const [loading, setLoading] = useState(true);
-const {data, loading} = FetchData()
+  const { data, loading } = FetchData();
+  const [activeTab, setActiveTab] = useState('ECC');
 
-const formatDate = (timestamp) => {
-  const date = new Date(timestamp.seconds * 1000); // Convert from seconds to milliseconds
-  return date.toLocaleDateString('en-US');
-};
+  const formatDate = (timestamp) => {
+    const date = new Date(timestamp.seconds * 1000); // Convert from seconds to milliseconds
+    return date.toLocaleDateString('en-US');
+  };
 
-// useEffect(() => {
-//   if (data) {
-//     setLoading(false); // Set loading to false when data is available
-//   }
-// }, [data]);
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
 
-if (loading) {
-  return <div className='flex-1 grid place-items-center'><i className="fa-solid fa-spinner animate-spin text-6xl"></i></div>;
-}
+  if (loading) {
+    return <div className='flex-1 grid place-items-center'><i className="fa-solid fa-spinner animate-spin text-6xl"></i></div>;
+  }
 
-    return (
-        <div className='w-full max-w-[65ch] mx-auto flex flex-col gap-3 sm:gap-5'>
-      <div>
-        <h3>ECC</h3>
-        <p>Date Issued: {formatDate(data.ECC['Date Issued'])}</p>
-        <p>ECC No.: {data.ECC['ECC No. ']}</p>
-        <p>Status: {data.ECC.Status}</p>
+  return (
+    <div className='flex flex-col items-center'>
+      <div className='flex gap-2'>
+        <button
+          className={`px-4 py-2 rounded-lg font-bold focus:outline-none ${activeTab === 'ECC' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+          onClick={() => handleTabChange('ECC')}
+        >
+          ECC
+        </button>
+        <button
+          className={`px-4 py-2 rounded-lg font-bold focus:outline-none ${activeTab === 'PTO' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+          onClick={() => handleTabChange('PTO')}
+        >
+          PTO
+        </button>
+        <button
+          className={`px-4 py-2 rounded-lg font-bold focus:outline-none ${activeTab === 'DP' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+          onClick={() => handleTabChange('DP')}
+        >
+          DP
+        </button>
+        <button
+          className={`px-4 py-2 rounded-lg font-bold focus:outline-none ${activeTab === 'SMR' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+          onClick={() => handleTabChange('SMR')}
+        >
+          SMR
+        </button>
+        <button
+          className={`px-4 py-2 rounded-lg font-bold focus:outline-none ${activeTab === 'CMR' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+          onClick={() => handleTabChange('CMR')}
+        >
+          CMR
+        </button>
       </div>
-      <div>
-        <h3>PTO</h3>
-        <p>Date Issued: {formatDate(data.PTO['Date Issued'])}</p>
-        <p>Expiry Date: {formatDate(data.PTO['Expiry Date'])}</p>
-        <p>Permit No.: {data.PTO['Permit No.']}</p>
-        <p>Status: {data.PTO['Status']}</p>
-      </div>
-      <div>
-        <h3>DP</h3>
-        <p>Date Issued: {formatDate(data.DP['Date Issued'])}</p>
-        <p>Expiry Date: {formatDate(data.DP['Expiry Date'])}</p>
-        <p>Permit No.: {data.DP['Permit No.']}</p>
-        <p>Status: {data.DP['Status']}</p>
-      </div>
-      <div>
-        <h3>SMR</h3>
-        <p>Next Monitoring Period: {formatDate(data.SMR['Next Monitoring Period'].Date)}</p>
-        <p>Status: {data.SMR['Next Monitoring Period'].Status}</p>
-        <p>Previous Monitoring Period: {formatDate(data.SMR['Prev Monitoring Period'].Date)}</p>
-        <p>Status: {data.SMR['Prev Monitoring Period'].Status}</p>
-      </div>
-      <div>
-        <h3>CMR</h3>
-        <p>Next Monitoring Period: {formatDate(data.CMR['Next Monitoring Period'].Date)}</p>
-        <p>Status: {data.CMR['Next Monitoring Period'].Status}</p>
-        <p>Previous Monitoring Period: {formatDate(data.CMR['Prev Monitoring Period'].Date)}</p>
-        <p>Status: {data.CMR['Prev Monitoring Period'].Status}</p>
-      </div>
-      
+
+      <div className='flex flex-col items-center'>
+      {activeTab === 'ECC' && (
+        <div>
+          <h3 className='font-semibold text-lg'>Environmental Compliance Certificate</h3>
+          <p>Date Issued: {formatDate(data.ECC['Date Issued'])}</p>
+          <p>ECC No.: {data.ECC['ECC No. ']}</p>
+          <p>Status: {data.ECC.Status}</p>
         </div>
-    )
+      )}
+
+      {activeTab === 'PTO' && (
+        <div>
+          <h3 className='font-semibold text-lg'>Permit to Operate</h3>
+          <p>Date Issued: {formatDate(data.PTO['Date Issued'])}</p>
+          <p>Expiry Date: {formatDate(data.PTO['Expiry Date'])}</p>
+          <p>Permit No.: {data.PTO['Permit No.']}</p>
+          <p>Status: {data.PTO.Status}</p>
+        </div>
+      )}
+      
+      {activeTab === 'DP' && (
+        <div>
+          <h3 className='font-semibold text-lg'>Discharge Permit</h3>
+          <p>Date Issued: {formatDate(data.DP['Date Issued'])}</p>
+          <p>Expiry Date: {formatDate(data.DP['Expiry Date'])}</p>
+          <p>Permit No.: {data.DP['Permit No.']}</p>
+          <p>Status: {data.DP.Status}</p>
+        </div>
+      )}
+      
+      {activeTab === 'SMR' && (
+        <div>
+          <h3 className='font-semibold text-lg'>Self Monitoring Report</h3>
+          <p>Next Monitoring Period: {formatDate(data.SMR['Next Monitoring Period'].Date)}</p>
+          <p>Status: {data.SMR['Next Monitoring Period'].Status}</p>
+          <p>Previous Monitoring Period: {formatDate(data.SMR['Prev Monitoring Period'].Date)}</p>
+          <p>Status: {data.SMR['Prev Monitoring Period'].Status}</p>
+        </div>
+      )}
+      
+      {activeTab === 'CMR' && (
+        <div>
+          <h3 className='font-semibold text-lg'>Compliance Monitoring Report</h3>
+          <p>Next Monitoring Period: {formatDate(data.CMR['Next Monitoring Period'].Date)}</p>
+          <p>Status: {data.CMR['Next Monitoring Period'].Status}</p>
+          <p>Previous Monitoring Period: {formatDate(data.CMR['Prev Monitoring Period'].Date)}</p>
+          <p>Status: {data.CMR['Prev Monitoring Period'].Status}</p>
+        </div>
+      )}
+      
+      </div>
+    </div>
+  );
 }
 
 export default UserDashboard;
